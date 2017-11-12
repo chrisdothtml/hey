444c94c324e2f1070f132a38f45a37101334389d
Chris Deacy <chrisdothtml@gmail.com>
2017-11-12 17:54:29 -0600

    adding test for `in` modifier; renaming test file

--- test/parse-command.js
+++ test/parse-command.js
@@ -5,12 +5,13 @@ const { expect } = require('chai')
 
 describe('parseCommand', () => {
   it('extracts the command type, args, and modifiers', () => {
-    const input = 'copy file.txt *.js from ../dir into dir/thing to new-dir'
+    const input = 'copy file.txt *.js in some-dir from ../dir into dir/thing to new-dir'
     const parsed = parseCommand(input.split(' '))
     const expected = {
       args: ['file.txt', '*.js'],
       mods: {
         from: '../dir',
+        in: 'some-dir',
         into: 'dir/thing',
         to: 'new-dir'
       },
